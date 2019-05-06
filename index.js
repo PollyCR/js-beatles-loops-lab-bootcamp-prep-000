6ccdb696e912fc70c2683b6a8c9b44032fe9d646
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 21:19:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,18 +9,15 @@ function theBeatlesPlay(musicians,instruments) {
   return phrases;
 }
 
-function johnLennonFacts() {
-  var facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-var newFacts = [];
-var i = 0
-while (i < facts.length) {
-  newFacts.push(facts[i] + "!!!")
-  i++;
-}
-return newFacts
-}
+var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout",
+"He hated the sound of his own voice"];
+
+//alters the array when called
+function johnLennonFacts(facts) {
+  var i = 0;
+    while (i < facts.length) {
+       facts[i] += "!!!";
+     i++;
+    }
+    console.log(facts);
+  }
