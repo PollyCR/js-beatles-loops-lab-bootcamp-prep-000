a00a2c905730b47e40eae7047f257bb18e1c9a06
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 20:52:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const musicians = ["John Lennon", "Paul McCartney", "Ringo Starr", "George Harrison"]
 const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"]
 function theBeatlesPlay(musicians,instruments) {
-  var = [""]
+  var = [];
 }
