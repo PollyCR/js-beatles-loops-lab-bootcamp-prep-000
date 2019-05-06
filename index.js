48889c88847cee735f6da3c055f741110fed7355
exuberant-object-0311 <polly.c.reynolds@gmail.com>
2019-05-06 21:08:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,5 +10,11 @@ function theBeatlesPlay(musicians,instruments) {
 }
 
 function johnLennonFacts() {
-  var facts = []
+  var facts = [
+  "He was the last Beatle to learn to drive",
+  "He was never a vegetarian",
+  "He was a choir boy and boy scout",
+  "He hated the sound of his own voice"
+];
+while 
 }
